50311438b7fcbb4e764be6ccbd962394370ffa98
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2018-08-20 22:41:10 +0200

    Validate and cancel work

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,9 +19,16 @@ before_action :set_order, only: [:show, :edit, :update]
   end
 
   def update
-    @order.update(order_params)
-    @order.save
-    redirect_to order_path
+    if order_params[:status].to_i == 1
+      @order.update(order_params)
+      @order.save
+      redirect_to order_path
+    else
+      @order.product_orders.destroy_all
+      @order.update(order_params)
+      @order.save
+      redirect_to products_path
+    end
   end
 
   def destroy
