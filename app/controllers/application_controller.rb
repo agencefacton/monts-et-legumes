36051b89f4f223607604c051cf8774bd7bd3f947
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2018-08-27 23:31:44 +0200

    custom registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,20 @@ class ApplicationController < ActionController::Base
     helper_method :current_order
   # before_action :authenticate_user!
   # include Pundit
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # rescue_from Pundit: :NotAuthorizedError, with: :user_not_authorized
 
-  # def configure_permitted_parameters
-  #   # For additional fields in app/views/devise/registrations/new.html.erb
-  #  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
 
-  #   # For additional in app/views/devise/registrations/edit.html.erb
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  # end
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users
+  # , :controllers => { registrations: 'registrations' }
   # get 'orders/:id/reset_status', to: 'orders#reset_status', :as => "reset_status"
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
