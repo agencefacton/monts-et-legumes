7237f3815ccc44e8b9ee696b12b0a837ad5a1c18
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2018-09-06 17:04:32 +0200

    navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     helper_method :current_order
-  # before_action :authenticate_user!
-  # include Pundit
+
+    before_action :authenticate_user!
+    include Pundit
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+    after_action :verify_authorized, except: :index, unless: :skip_pundit?
+    after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  # rescue_from Pundit: :NotAuthorizedError, with: :user_not_authorized
+    # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -17,6 +18,11 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
   end
+
+  # def user_not_authorized
+  #   flash[:alert] = "Connectez-vous pour continuer"
+  #   redirect_to(root_path)
+  # end
 
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,29 @@ User.create(
   password: 'azerty',
   admin: true
 )
+
+User.create(
+  email: 'client@fidele.com',
+  first_name: 'Client',
+  last_name: 'Fidèle',
+  phone_number: '0000000000',
+  password: 'azerty',
+  admin: false
+)
+
+Order.create(
+  week_number: 12,
+  total_price: 20
+  user_id: 1
+)
+
+Product.create(
+  name: "Carottes",
+  price: 2,
+  quantity: "Kilo")
+
+
+Product.create(
+  name: "Romaric le basilic",
+  price: 3,
+  quantity: "Pièce")
