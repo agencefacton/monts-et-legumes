61dc561c3ec60f68aab65c6e36a118ecf35d02be
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-02-03 16:41:37 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,51 @@
-# User.destroy_all
+User.destroy_all
+Order.destroy_all
+Product.destroy_all
 
-# User.create(
-#   email: 'tiphanietourniaire@hotmail.com',
-#   first_name: 'Tiphanie',
-#   last_name: 'Tourniaire',
-#   phone_number: '0000000000',
-#   password: 'azerty',
-#   admin: true
-# )
+tiph = User.create!(
+  email: 'tiphanietourniaire@hotmail.com',
+  first_name: 'Tiphanie',
+  last_name: 'Tourniaire',
+  phone_number: '0000000000',
+  password: 'azerty',
+  admin: true
+)
 
-# User.create(
-#   email: 'client@fidele.com',
-#   first_name: 'Client',
-#   last_name: 'Fidèle',
-#   phone_number: '0000000000',
-#   password: 'azerty',
-#   admin: false
-# )
+client = User.create!(
+  email: 'client@fidele.com',
+  first_name: 'Client',
+  last_name: 'Fidèle',
+  phone_number: '0000000000',
+  password: 'azerty',
+  admin: false
+)
+
+Product.create!(
+  name: "Carottes",
+  price: 2,
+  unit: "kilo",
+  category: "Légumes"
+)
+
+Product.create!(
+  name: "Aubergines",
+  price: 4.5,
+  unit: "kilo",
+  category: "Légumes"
+)
+
+Product.create!(
+  name: "Choux",
+  price: 2.5,
+  unit: "pièce",
+  category: "Légumes"
+)
 
 Order.create(
   year_number: 2018,
   week_number: 12,
   total_price: 20,
-  user_id: 1,
+  user: tiph,
   status: 1
 )
 
@@ -30,17 +53,6 @@ Order.create(
   year_number: 2019,
   week_number: 3,
   total_price: 21.5,
-  user_id: 4,
+  user_id: client,
   status: 1
-  )
-
-# Product.create(
-#   name: "Carottes",
-#   price: 2,
-#   unit: "Kilo")
-
-
-# Product.create(
-#   name: "Romaric le basilic",
-#   price: 3,
-#   unit: "Pièce")
+)
