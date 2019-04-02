3f869f18ad0b74ccee4251579a84b43f6f6202ea
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-04-02 22:43:03 +0200

    Set order#status default value to 0

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_04_01_211237) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.boolean "editable", default: false
     t.integer "year_number"
     t.index ["user_id"], name: "index_orders_on_user_id"
