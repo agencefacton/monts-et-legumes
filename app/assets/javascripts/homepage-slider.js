f1f7a755ee5fa921ed030d2afdd06e6c06d6c7e3
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-04-08 21:45:51 +0200

    Init slider only if present in page

--- app/assets/javascripts/homepage-slider.js
+++ app/assets/javascripts/homepage-slider.js
@@ -1,26 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const nextButton = document.querySelector('.button-next');
-//   const prevButton = document.querySelector('.button-prev');
-
-//   nextButton.addEventListener('click', function() {
-//     const current = document.querySelector('.slide.active');
-//     const next = current.nextElementSibling;
-//     if (next) {
-//       current.classList.remove('active');
-//       next.classList.add('active');
-//     }
-//   });
-
-//   prevButton.addEventListener('click', function() {
-//     const current = document.querySelector('.slide.active');
-//     const prev = current.previousElementSibling;
-//     if (prev) {
-//       current.classList.remove('active');
-//       prev.classList.add('active');
-//     }
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   var button1 = document.querySelector('.button1');
   var button2 = document.querySelector('.button2');
@@ -28,63 +5,73 @@ document.addEventListener('DOMContentLoaded', function() {
   var button4 = document.querySelector('.button4');
   var button5 = document.querySelector('.button5');
 
-  button1.addEventListener('click', function() {
-    var current = document.querySelector('.active');
-    var btnactive = document.querySelector('.btnactive')
-    var slide = document.querySelector('.slide1');
-    if (slide) {
-      current.classList.remove('active');
-      btnactive.classList.remove('btnactive')
-      button1.classList.add('btnactive')
-      slide.classList.add('active');
-    }
-  });
+  if (button1) {
+    button1.addEventListener('click', function() {
+      var current = document.querySelector('.active');
+      var btnactive = document.querySelector('.btnactive')
+      var slide = document.querySelector('.slide1');
+      if (slide) {
+        current.classList.remove('active');
+        btnactive.classList.remove('btnactive')
+        button1.classList.add('btnactive')
+        slide.classList.add('active');
+      }
+    });
+  }
 
-  button2.addEventListener('click', function() {
-    var current = document.querySelector('.active');
-    var slide = document.querySelector('.slide2');
-    var btnactive = document.querySelector('.btnactive')
-    if (slide) {
-      current.classList.remove('active');
-      btnactive.classList.remove('btnactive')
-      button2.classList.add('btnactive')
-      slide.classList.add('active');
-    }
-  });
+  if (button2) {
+    button2.addEventListener('click', function() {
+      var current = document.querySelector('.active');
+      var slide = document.querySelector('.slide2');
+      var btnactive = document.querySelector('.btnactive')
+      if (slide) {
+        current.classList.remove('active');
+        btnactive.classList.remove('btnactive')
+        button2.classList.add('btnactive')
+        slide.classList.add('active');
+      }
+    });
+  }
 
-  button3.addEventListener('click', function() {
-    var current = document.querySelector('.active');
-    var slide = document.querySelector('.slide3');
-    var btnactive = document.querySelector('.btnactive')
-    if (slide) {
-      current.classList.remove('active');
-      btnactive.classList.remove('btnactive')
-      button3.classList.add('btnactive')
-      slide.classList.add('active');
-    }
-  });
+  if (button3) {
+    button3.addEventListener('click', function() {
+      var current = document.querySelector('.active');
+      var slide = document.querySelector('.slide3');
+      var btnactive = document.querySelector('.btnactive')
+      if (slide) {
+        current.classList.remove('active');
+        btnactive.classList.remove('btnactive')
+        button3.classList.add('btnactive')
+        slide.classList.add('active');
+      }
+    });
+  }
 
+  if (button4) {
     button4.addEventListener('click', function() {
-    var current = document.querySelector('.active');
-    var slide = document.querySelector('.slide4');
-    var btnactive = document.querySelector('.btnactive')
-    if (slide) {
-      current.classList.remove('active');
-      btnactive.classList.remove('btnactive')
-      button4.classList.add('btnactive')
-      slide.classList.add('active');
-    }
-  });
+      var current = document.querySelector('.active');
+      var slide = document.querySelector('.slide4');
+      var btnactive = document.querySelector('.btnactive')
+      if (slide) {
+        current.classList.remove('active');
+        btnactive.classList.remove('btnactive')
+        button4.classList.add('btnactive')
+        slide.classList.add('active');
+      }
+    });
+  }
 
+  if (button5) {
     button5.addEventListener('click', function() {
-    var current = document.querySelector('.active');
-    var slide = document.querySelector('.slide5');
-    var btnactive = document.querySelector('.btnactive')
-    if (slide) {
-      current.classList.remove('active');
-      btnactive.classList.remove('btnactive')
-      button5.classList.add('btnactive')
-      slide.classList.add('active');
-    }
-  });
+      var current = document.querySelector('.active');
+      var slide = document.querySelector('.slide5');
+      var btnactive = document.querySelector('.btnactive')
+      if (slide) {
+        current.classList.remove('active');
+        btnactive.classList.remove('btnactive')
+        button5.classList.add('btnactive')
+        slide.classList.add('active');
+      }
+    });
+  }
 });
