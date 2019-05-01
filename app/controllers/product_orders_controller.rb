c49ce903b9e40480949bb4dd2ede2a289913a7e2
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-05-01 20:34:24 +0200

    Fix bad links and other stuff on client side

--- app/controllers/product_orders_controller.rb
+++ app/controllers/product_orders_controller.rb
@@ -3,23 +3,6 @@ class ProductOrdersController < ApplicationController
   before_action :set_order, only: [:create, :destroy, :update]
   before_action :set_products, only: [:create, :update]
 
-  def index
-    @years = Order.where(status: 1).group(:year_number).count
-  end
-
-  def show
-    @year = params[:year_number].to_i
-    @weeks = Order.where(status: 1, year_number: @year).group(:week_number).count
-    @categories = Category.all
-  end
-
-  def week
-    @year = params[:year_number].to_i
-    @week = params[:week_number].to_i
-    @categories = Category.all
-    @quantities = ProductOrder.joins(:product, :order).where("status = ?", 1).where("year_number = #{@year}").where("week_number = #{@week}").group(:product).sum(:quantity)
-  end
-
   def create
     @categories = Category.all
     if product_order_params[:quantity].to_i > 0
