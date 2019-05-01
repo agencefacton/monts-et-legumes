833bb309607758c6922b751335f1bd4960f61858
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-05-01 18:22:19 +0200

    Remove useless products actions for client side

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,64 +1,9 @@
 class ProductsController < ApplicationController
-before_action :set_product, only: [:show, :edit, :update, :destroy, :change_active]
-
   def index
-    if current_user.admin?
-      @products = Product.order(active: :desc)
-    else
-      @products = Product.where(active: true)
-    end
+    @products = Product.where(active: true)
     @product_orders = current_order.product_orders
     @order = current_order
-    @product = Product.new
     @categories = Category.all
     @post = Post.last
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :index
-    end
-  end
-
-  def change_active
-    @product.active = !@product.active
-    @product.save
-    redirect_to products_path
-  end
-
-  def edit
-  end
-
-  def update
-    @product.update(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @product.destroy
-    redirect_to products_path
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price, :unit, :category_id, :description, :active, :photo)
-  end
-
-  def set_product
-    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :product_orders, except: [:index, :show]
   resources :product_orders, only: [:index, :show], param: :year_number, as: "date"
   resources :customer_orders, only: [:index, :show], param: :year_number, as: "customer_orders_year"
-  resources :products
+  resources :products, only: :index
 
   get "/product_orders/:year_number/:week_number", to: "product_orders#week", as: "week"
   get "/customer_orders/:year_number/:week_number", to: "customer_orders#week", as: "customer_orders_week"
