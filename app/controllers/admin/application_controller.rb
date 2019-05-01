1ee209d9ba6aade289986850611e70c1ff0c2e4d
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-05-01 18:08:32 +0200

    Extract admin navbar

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,8 @@ module Admin
   class ApplicationController < ::ApplicationController
     before_action :authenticate_admin!
 
+    layout 'admin'
+
     def authenticate_admin!
       redirect_to root_path unless current_user.admin?
     end
