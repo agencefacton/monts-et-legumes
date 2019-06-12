5ab6471d2e3d31304079cff403e79df06cd004b0
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2019-06-12 23:50:25 +0200

    sales by year

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -6,6 +6,10 @@ module Admin
                              .where(status: 1)
                              .group("DATE_TRUNC('year', selling_ranges.ends_at)")
                              .count
+      @sales_by_year =  Order.joins(:selling_range)
+                             .where(status: 1)
+                             .group("DATE_TRUNC('year', selling_ranges.ends_at)")
+                             .sum(:total_price)
       @categories = Category.order(id: :asc)
     end
 
