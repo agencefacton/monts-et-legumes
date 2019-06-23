02577dcd3920f5da581456bb3d73e8fc599d56aa
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2019-06-23 18:49:26 +0200

    remove tel mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :phone_number
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
